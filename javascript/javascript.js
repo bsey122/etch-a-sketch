ce9162ced74d737bbe698a8c7dd32fcb39dd1fb3
Brandon Seymour <bseymour122@yahoo.ca>
2022-03-05 14:26:29 -0500

    Create function to get input from slider

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,15 +1,32 @@
 const container = document.querySelector('.drawing-pad');
-for (let index = 0; index < 16; index++) {
-    for (let index = 0; index < 16; index++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square); 
+const slider = document.querySelector('#grid-size');
+makeGrid();
+function removeNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+function makeGrid(e) {
+    let sliderValue = slider.value;
+    console.log(slider.value)
+    removeNodes(container);
+    container.setAttribute('style', `grid-template-columns: repeat(${sliderValue}, 1fr)`);
+    for (let index = 0; index < sliderValue; index++) {
+        for (let index = 0; index < sliderValue; index++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            container.appendChild(square); 
+        }  
     }  
 }
 function black(e) {
     e.target.style.background = 'black';
 }
-const gridElement = document.querySelectorAll('.square');
-gridElement.forEach((gridElem) => {
-    gridElem.addEventListener('mouseover', black);
+container.addEventListener('mouseover', function (e) {
+    if (e.target.classList.contains('square')) {
+        black(e);
+    }
+});
+slider.addEventListener('input', function (e) {
+    makeGrid();
 });
