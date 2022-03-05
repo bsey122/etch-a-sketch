4963a92162c13ea123c9a36a36206140505eddb8
Brandon Seymour <bseymour122@yahoo.ca>
2022-03-04 19:29:28 -0500

    Add querySelector to change squares black

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,15 @@
 const container = document.querySelector('.drawing-pad');
 for (let index = 0; index < 16; index++) {
     for (let index = 0; index < 16; index++) {
-        const square = document.createElement('div');
+        let square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square); 
     }  
 }
+function black(e) {
+    e.target.style.background = 'black';
+}
+const gridElement = document.querySelectorAll('.square');
+gridElement.forEach((gridElem) => {
+    gridElem.addEventListener('mouseover', black);
+});
